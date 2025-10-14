2e5acc3020bc30ebddd3a752ab01fa0a73caa4fa
Shine <shine.sl@sectorqube.com>
2025-10-14 10:00:28 +0530

    first commit

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar'
+import { HeaderActions } from "../header-actions/header-actions";
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbar, HeaderActions],
+  template: `
+<mat-toolbar class="w-full elevated py-2">
+  <div class="max-w-[1200px] mx-auto w-full flex items-center justify-between"> <span>Modern Store</span> 
+      <app-header-actions />
+  </div>
+
+
+  `,
+  styles: ``
+})
+export class Header {
+
+}
---
+++ src/app/pages/product-grid/product-grid.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-grid',
+  imports: [],
+  template: `
+    <p>
+      product-grid works!
+    </p>
+  `,
+  styles: ``
+})
+export default class ProductGrid {
+
+}
