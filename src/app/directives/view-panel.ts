ccdc8e8eaf0e35794cdf44440b84becf54e664a7
Shine <shine.sl@sectorqube.com>
2025-11-28 16:28:34 +0530

    dummy data offer added

--- src/app/directives/view-panel.ts
+++ src/app/directives/view-panel.ts
@@ -3,7 +3,7 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: '[appViewPanel]',
   host:{
-    class:'border border-gray-200 rounded-xl p-6 bg-white'
+    class:'border border-[#009774] rounded-xl p-6 bg-white'
   }
 })
 export class ViewPanel {
--- src/app/pages/offers/offers.ts
+++ src/app/pages/offers/offers.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-offers',
   imports: [],
   template: `
-    <p>
-      offers works!
-    </p>
+<div class="mb-4 transition-all ease-out  duration 200 hover:-translate-x-1">
+  <img [src]="image().name" alt="">
+</div>
   `,
   styles: ``
 })
 export class Offers {
 
+  image = signal(objects[0]) ;
+  ngOnInit(){
+    this.carousal(objects)
+  }
+  carousal(offers:any){
+    let index =1;
+    setInterval(()=> {
+      this.image.set(offers[index]) 
+      index = (index +1)% offers.length;      
+    },3000)
+  }
 }
+
+const objects = [
+  { id: 1, name: "offer1.webp" },
+  { id: 2, name: "offer2.webp" },
+];
