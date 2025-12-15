9d26722a42e994ed276d154fd044209949d4c658
Shine <shine.sl@sectorqube.com>
2025-12-15 12:10:09 +0530

    logo update

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -6,7 +6,9 @@ import { HeaderActions } from "../header-actions/header-actions";
   imports: [HeaderActions, MatToolbar],
   template: `
 <mat-toolbar class="w-full elevated py-2">
-  <div class=" mx-auto w-full flex items-center justify-between"> <span>
+  <div class=" mx-auto w-full flex items-center justify-between">
+  <span class="flex items-center gap-3">
+    <img src="life-styles.png" alt="logo" class="w-8 h-8 " alt="">
 <h1 class="text-5xl md:text-7xl font-extrabold tracking-tight leading-tight bg-gradient-to-r from-emerald-600 via-teal-600 to-slate-800 bg-clip-text text-transparent">
   Life Styles
 </h1>
